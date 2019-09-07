6004b925cbde48afe11c0f09f63689a8bbabe627
Chris (Someguy123) <info@someguy123.com>
2019-09-07 03:22:48 +0100

    version 1.1.2 - split up batch calls into sub-batches

--- balancer/app.py
+++ balancer/app.py
@@ -18,12 +18,16 @@ Copyright::
 """
 import asyncio
 import json
+import math
 from datetime import datetime
+from json import JSONDecodeError
 from typing import Union
 from urllib.parse import urlparse
+
+import requests
 from quart import Quart, request, jsonify
 from privex.helpers import empty
-from privex.jsonrpc import JsonRPC
+from privex.jsonrpc import JsonRPC, RPCException
 from werkzeug.exceptions import BadRequest
 import logging
 from balancer.node import find_endpoint, Endpoint
@@ -53,26 +57,97 @@ async def extract_json(rq: request):
             return json.loads(data[0])
         raise e
 
+rs = requests.Session()
 
-async def make_call(method, params):
-    endpoint = find_endpoint(method)  # type: Endpoint
-    uri = urlparse(endpoint.host)
-    port = uri.port if uri.port is not None else 443 if uri.scheme == 'https' else 80
 
-    j = JsonRPC(uri.hostname, port=port, ssl=(uri.scheme == 'https'))
+async def json_call(url, method, params, jid=1, timeout=120):
+    headers = {'content-type': 'application/json'}
+
+    payload = {
+        "method": method,
+        "params": params,
+        "jsonrpc": "2.0",
+        "id": jid,
+    }
+    r = None
     try:
-        if type(params) is dict:
-            return j.call(method, **params), endpoint
-        elif type(params) is list:
-            return j.call(method, *params), endpoint
-        else:
-            raise AttributeError('JSON Params was neither dict nor list...')
-    except AttributeError as e:
+        log.debug('Sending JsonRPC request to %s with payload: %s', url, payload)
+        r = rs.post(url, data=json.dumps(payload), headers=headers, timeout=timeout)
+        r.raise_for_status()
+        response = r.json()
+    except JSONDecodeError as e:
+        log.warning('JSONDecodeError while querying %s', url)
+        log.warning('Params: %s', params)
+        t = r.text.decode('utf-8') if type(r.text) is bytes else str(r.text)
+        log.warning('Raw response data was: %s', t)
         raise e
+
+    return response
+
+
+async def json_list_call(url, data: list, timeout=120):
+    headers = {'content-type': 'application/json'}
+    r = rs.post(url, data=json.dumps(data), headers=headers, timeout=timeout)
+    r.raise_for_status()
+    response = r.json()
+    return response
+
+
+
+
+async def make_batch_call(method, data):
+    endpoint = find_endpoint(method)  # type: Endpoint
+    try:
+        return await json_list_call(endpoint.host, data), endpoint
     except Exception as e:
         raise EndpointException(
             f'Error while calling {method} on {endpoint.host} - reason: {type(e)} {str(e)}', endpoint=endpoint
         )
+
+
+async def make_call(method, params, jid=1):
+    endpoint = find_endpoint(method)  # type: Endpoint
+    # uri = urlparse(endpoint.host)
+    # port = uri.port if uri.port is not None else 443 if uri.scheme == 'https' else 80
+
+    # j = JsonRPC(uri.hostname, port=port, ssl=(uri.scheme == 'https'))
+    try:
+        return await json_call(endpoint.host, method=method, params=params, jid=jid), endpoint
+    except Exception as e:
+        raise EndpointException(
+            f'Error while calling {method} on {endpoint.host} - reason: {type(e)} {str(e)}', endpoint=endpoint
+        )
+
+
+async def filter_methods(data: list):
+    methods = {}
+    for d in data:
+        m = d['method']
+        if m not in methods:
+            methods[m] = []
+        methods[m].append(d)
+    return methods
+
+
+def chunked(iterable, n):
+    """ Split iterable into ``n`` iterables of similar size
+
+    Examples::
+        >>> l = [1, 2, 3, 4]
+        >>> list(chunked(l, 4))
+        [[1], [2], [3], [4]]
+
+        >>> l = [1, 2, 3]
+        >>> list(chunked(l, 4))
+        [[1], [2], [3], []]
+
+        >>> l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        >>> list(chunked(l, 4))
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]
+
+    """
+    chunksize = int(math.ceil(len(iterable) / n))
+    return (iterable[i * chunksize:i * chunksize + chunksize] for i in range(n))
 
 
 @flask.route('/', methods=['GET', 'POST'])
@@ -92,15 +167,28 @@ async def index():
         log.debug('JSON Request: %s', data)
 
         if type(data) is dict:
+            # data = [data]
             method = data['method']  # type: str
             params = data.get('params', [])  # type: Union[dict, list]
             call_list = [make_call(method=method, params=params)]
         elif type(data) is list:
             if len(data) > MAX_BATCH:
                 return jsonify(error=True, message=f"Too many batch calls. Max batch calls is: {MAX_BATCH}")
-            call_list = [make_call(method=d['method'], params=d.get('params', [])) for d in data]
-        else:
-            raise Exception("JSON data was not dict or list.")
+            call_dict = await filter_methods(data)
+            call_chunks = []
+            for meth, rq in call_dict.items():
+                mcl = call_dict[meth]
+                chunk_size = math.ceil(len(mcl) / 20) if len(mcl) > 20 else 1
+                call_chunks += list(chunked(call_dict[meth], chunk_size))
+
+            log.info(call_chunks)
+
+            call_list = []
+            for c in call_chunks:
+                call_list.append(make_batch_call(c[0]['method'], c))
+
+        # else:
+        #     raise Exception("JSON data was not dict or list.")
     except Exception as e:
         log.warning('Could not parse request. Returning error. Reason: %s %s', type(e), str(e))
         return jsonify(error=True, message="An error occurred while attempting to parse JSON request body..."), 400
@@ -118,13 +206,14 @@ async def index():
             resp = jsonify(jsonrpc='2.0', result=res, id=res.get('id', 1))
             resp.headers['X-Upstream'] = endpoint.host if empty(endpoint.name) else endpoint.name
         else:
-            res = [dict(jsonrpc='2.0', result=r[0], id=i+1) for i, r in enumerate(call_res)]
+            res = [r[0] for i, r in enumerate(call_res)]
             resp = jsonify(res)
             resp.headers['X-Upstream'] = 'Unknown due to batch call.'
         # resp = jsonify(jsonrpc='2.0', result=res, id=data.get('id', 1))
         # resp.headers['X-Upstream'] = endpoint.host if empty(endpoint.name) else endpoint.name
         return resp
-    except AttributeError:
+    except AttributeError as e:
+        log.exception('attribute error')
         return jsonify(error=True, message="Incorrectly formatted 'params'. Must be list or dict"), 400
     except EndpointException as e:
         log.warning('Exception while calling JsonRPC server %s - reason: %s %s', e.endpoint, type(e), str(e))
